d0e68db090cf53d1c00d55673212b9ea06b62ee6
sagar kumar <94098920+Sagar-Kumar01@users.noreply.github.com>
2025-11-05 16:24:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+let likeness = document.getElementById("likeness");
+let show = document.getElementById("show");
+
+
+
+likeness.addEventListener("input",()=>{
+	show.textContent = likeness.value;
+})
